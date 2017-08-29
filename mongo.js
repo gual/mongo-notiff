8e0fec8d26501989975fe27a3ed37907690630b2
Gual Escalante <gual23@gmail.com>
2017-08-29 15:58:06 -0600

    Added filter params

--- mongo.js
+++ mongo.js
@@ -7,40 +7,52 @@ var MongoClient = require('mongodb').MongoClient
 
 var argv = require('minimist')(process.argv.slice(2));
 
-var url = argv['connection-string'] ? argv['connection-string'] : 'mongodb://localhost:27017';
+var getParamValueOrDefault = function(key, defaultValue) { return argv[key] ? argv[key] : defaultValue };
 
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, db) {
-	assert.equal(null, err);
-	console.log("Connected successfully to server");
+var url = getParamValueOrDefault('connection-string', 'mongodb://localhost:27017');
+var collectionName = getParamValueOrDefault('collection-name', 'test');
+var key = getParamValueOrDefault('key', 'status');
+var value = getParamValueOrDefault('value', 'OK');
+
+var findDocuments = function(db) {
+  var collection = db.collection(collectionName);
+  var filterObject = {};
+  filterObject[key] = value;
+
+  collection.find(filterObject).toArray(function(err, docs) {
+	assert.equal(err, null);
 
 	nodemailer.createTestAccount((err, account) => {
-	    let transporter = nodemailer.createTransport({
-	        host: 'smtp.ethereal.email',
-	        port: 587,
-	        secure: false,
-	        auth: {
-	            user: account.user,
-	            pass: account.pass
-	        }
-	    });
+		let transporter = nodemailer.createTransport({
+			host: 'smtp.ethereal.email',
+			port: 587,
+			secure: false,
+			auth: {
+				user: account.user,
+				pass: account.pass
+			}
+		});
 
-	    let mailOptions = {
-	        from: '"Fred Foo 👻" <foo@blurdybloop.com>',
-	        to: 'gual23@gmail',
-	        subject: 'Hello ✔',
-	        text: 'Hello world?',
-	        html: '<b>Hello world?</b>'
-	    };
+		let mailOptions = {
+			from: '"Fred Foo 👻" <foo@blurdybloop.com>',
+			to: 'gual23@gmail',
+			subject: 'Hello ✔',
+			text: 'Found ' + docs.length + ' elements',
+		};
 
-	    transporter.sendMail(mailOptions, (error, info) => {
-	        if (error) {
-	            return console.log(error);
-	        }
-	        console.log('Message sent: %s', info.messageId);
-	        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-	    });
+		transporter.sendMail(mailOptions, (error, info) => {
+			if (error) {
+				return console.log(error);
+			}
+			console.log('Message sent: %s', info.messageId);
+			console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+		});
 	});
+  });
+}
 
+MongoClient.connect(url, function(err, db) {
+	assert.equal(null, err);
+	findDocuments(db);
 	db.close();
 });
