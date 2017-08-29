b0bd987ffe9ccdc03b260b68647afd1387511aa8
Gual Escalante <gual23@gmail.com>
2017-08-29 15:19:03 -0600

    Added email sending functionality

--- mongo.js
+++ mongo.js
@@ -1,3 +1,7 @@
+'use strict';
+
+const nodemailer = require('nodemailer');
+
 var MongoClient = require('mongodb').MongoClient
   , assert = require('assert');
 
@@ -7,9 +11,36 @@ var url = argv['connection-string'] ? argv['connection-string'] : 'mongodb://loc
 
 // Use connect method to connect to the server
 MongoClient.connect(url, function(err, db) {
-	console.dir(argv);
 	assert.equal(null, err);
 	console.log("Connected successfully to server");
+
+	nodemailer.createTestAccount((err, account) => {
+	    let transporter = nodemailer.createTransport({
+	        host: 'smtp.ethereal.email',
+	        port: 587,
+	        secure: false,
+	        auth: {
+	            user: account.user,
+	            pass: account.pass
+	        }
+	    });
+
+	    let mailOptions = {
+	        from: '"Fred Foo 👻" <foo@blurdybloop.com>',
+	        to: 'gual23@gmail',
+	        subject: 'Hello ✔',
+	        text: 'Hello world?',
+	        html: '<b>Hello world?</b>'
+	    };
+
+	    transporter.sendMail(mailOptions, (error, info) => {
+	        if (error) {
+	            return console.log(error);
+	        }
+	        console.log('Message sent: %s', info.messageId);
+	        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+	    });
+	});
 
 	db.close();
 });
